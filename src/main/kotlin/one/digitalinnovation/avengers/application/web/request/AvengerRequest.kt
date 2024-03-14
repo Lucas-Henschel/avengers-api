7efe1c0470577cf79e0f77f6c5c504cc535f0ce4
Lucas-Henschel <lucas101henschel@gmail.com>
2024-03-14 10:20:22 -0300

    Implemented avenger req and res

---
+++ src/main/kotlin/one/digitalinnovation/avengers/application/web/request/AvengerRequest.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.avengers.application.web.request
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+data class AvengerRequest(
+  @field:NotNull
+  @field:NotBlank
+  @field:NotEmpty
+  val nick: String,
+  @field:NotNull
+  @field:NotBlank
+  @field:NotEmpty
+  val person: String,
+  val description: String? = "",
+  val history: String? = "",
+)
